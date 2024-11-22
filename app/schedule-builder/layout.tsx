0b4144a4939f91890fa4cd73b7283c1a8b484ae2
Cameron DeMille <camerondemille2153@gmail.com>
2024-11-22 00:39:54 -0600

    Fixed missing font file on schedule builder page

--- app/schedule-builder/layout.tsx
+++ app/schedule-builder/layout.tsx
@@ -1,17 +1,12 @@
 import '../globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
+import { Roboto } from 'next/font/google'
 
-const geistSans = localFont({
-  src: '../fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-})
-const geistMono = localFont({
-  src: '../fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
 })
 
 export const metadata: Metadata = {
@@ -29,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${roboto.className} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
